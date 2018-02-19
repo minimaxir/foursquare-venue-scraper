8e38c7aa9fcd8d774f6106a1a25fbe122dd0ea7e
Max Woolf <max@minimaxir.com>
2018-02-18 23:16:03 -0800

    Working venue scraper

--- foursquare_venues.py
+++ foursquare_venues.py
@@ -2,6 +2,9 @@ import yaml
 import requests
 import datetime
 import time
+import csv
+
+# Get all Venue IDs for venues within the bounding box.
 
 
 def get_intervals(lower, upper, length):
@@ -11,7 +14,6 @@ def get_intervals(lower, upper, length):
 
 with open("config.yaml", "r") as f:
     cfg = yaml.load(f)
-
 
 lats = get_intervals(cfg['top_bound'], cfg['bottom_bound'], cfg['grid_size'])
 longs = get_intervals(cfg['left_bound'], cfg['right_bound'], cfg['grid_size'])
@@ -40,3 +42,47 @@ for lat in lats:
         time.sleep(0.1)
 
 print('{} Unique Venues Scraped.'.format(len(venue_ids)))
+
+# Get and process the data for each unique Venue.
+
+venue_params = {
+    'client_id': cfg['client_id'],
+    'client_secret': cfg['client_secret'],
+    'v': '20180218'
+}
+
+venue_ids_list = list(venue_ids)   # cannot iterate a set, so must coerce list
+
+with open('foursquare_venues.csv', 'w', encoding='utf-8') as f:
+    writer = csv.writer(f)
+    writer.writerow(['id', 'name', 'categories', 'lat', 'long', 'num_checkins',
+                     'num_likes', 'price', 'rating',
+                     'num_ratings', 'url_venue', 'url_foursquare'])
+
+    for venue_id in venue_ids_list:
+        r = requests.get(
+            'https://api.foursquare.com/v2/venues/{}'.format(venue_id),
+            params=search_params)
+        venue = r.json()['response']['venue']
+
+        id = venue_id
+        name = venue.get('name', '')
+        lat = venue.get('location', {}).get('lat', '')
+        long = venue.get('location', {}).get('lng', '')
+        num_checkins = venue.get('stats', {}).get('checkinsCount', '')
+        num_likes = venue.get('likes', {}).get('count', '')
+        rating = venue.get('rating', '')
+        num_ratings = venue.get('ratingSignals', '')
+        price = venue.get('price', {}).get('tier')
+        url_venue = venue.get('url', '')
+        url_foursquare = venue.get('shortUrl', '')
+
+        categories = venue.get('categories', '')
+        if len(categories) > 0:
+            categories = ', '.join([x['name'] for x in categories])
+
+        writer.writerow([id, name, categories, lat, long, num_checkins,
+                         num_likes, price, rating,
+                         num_ratings, url_venue, url_foursquare])
+
+        time.sleep(0.1)
